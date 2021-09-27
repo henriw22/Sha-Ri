4047a3d7af6b6c4cc2a19de94bbb9f17829427d5
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-27 01:44:31 -0700

    import and render component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Canvas from "./components/Canvas/Canvas";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Canvas />
       </header>
     </div>
   );
