1921872929c3771f68fd04b6cde8dea4d9973ced
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-02 21:40:16 -0700

    update to add edit feature

--- react-front-end/src/components/Event/Event.jsx
+++ react-front-end/src/components/Event/Event.jsx
@@ -1,6 +1,23 @@
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+
 function Event(props) {
-  const { x, y, title } = props.event;
-  return <div style={{ position: "absolute", top: y, left: x }}>{title}</div>;
+  const { id, x, y } = props.event;
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: y,
+        left: x,
+        width: "25px",
+        height: "25px",
+        cursor: "pointer"
+      }}
+      onClick={() => props.onEdit(id)}
+    >
+      <StarBorderIcon />
+    </div>
+  );
 }
 
 export default Event;
